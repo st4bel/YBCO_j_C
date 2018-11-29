41b8ab28d4f518cdf6446e2ba95b6dec263d98af
st4bel <nasak.o@hotmail.de>
2018-11-29 15:55:00 +0100

    tryhard

--- j_C.py
+++ j_C.py
@@ -1,20 +1,56 @@
 import csv
 import matplotlib.pyplot as plt
+import numpy as np
 
-x=[]
-y=[]
+#x=[]
+#y=[]
 
-lol=list(csv.reader(open("S088_U(I)_B7_6mA.txt","rt"), delimiter="\t"))
-for row in lol:
-    if len(row) > 1:
-        x.append(float(row[0]))
-        y.append(float(row[1]))
+#lol=list(csv.reader(open("S088_U(I)_B7_6mA.txt","rt"), delimiter="\t"))
+#for row in lol:
+#    if len(row) > 1:
+#        x.append(float(row[0]))
+#        y.append(float(row[1]))
 
 
-plt.plot(x,y,label="hello")
+def readfile(filename):
+    x=[]
+    y=[]
+
+    lol=list(csv.reader(open(filename,"rt"), delimiter="\t"))
+    for row in lol:
+        if len(row) == 1:
+            header = row[0]
+        elif len(row) > 1:
+            x.append(float(row[0]))
+            y.append(float(row[1]))
+    return {"header":header,"x":x,"y":y}
+
+def getOffset(csv):
+    offset_from_average = np.mean(csv["y"])
+    zeros = []
+    for i in range(0,len(csv["x"])):
+        if csv["x"][i]==0:
+            zeros.append(csv["y"][i])
+    offset_from_zero = np.mean(zeros)
+    return {"average":offset_from_average,"zeros":offset_from_zero}
+
+def correctOffset(csv,offset):
+    for i in range(0,len(csv["y"])):
+        csv["y"][i]-=offset
+    return csv
+
+
+
+filename = "S088_U(I)_B7_6mA.txt"
+content = readfile(filename)
+print(getOffset(content))
+content = correctOffset(offset=getOffset(content)["zeros"], csv=content)
+
+
+plt.plot(content["x"],content["y"],label="U(I)")
 plt.xlabel('x')
 plt.ylabel('y')
-plt.title('Interesting Graph\nCheck it out')
+plt.title(filename)
 plt.legend()
-plt.savefig("test.png")
-plt.show
+#plt.savefig("test.png")
+plt.show()
