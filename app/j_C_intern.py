05e5fa2775adededde3c476b54c6bc149eebc7db
st4bel <nasak.o@hotmail.de>
2019-01-11 12:45:16 +0100

    close #2

--- app/j_C_intern.py
+++ app/j_C_intern.py
@@ -49,12 +49,14 @@ def ohmig_res(xdata,ydata):
     p=np.poly1d(np.polyfit(xdata,ydata,11))
     d2p=p.deriv(2)
     roots=d2p.r
-    cut=np.logical_and(xdata>=np.amin(roots),xdata<==np.amax(roots))
+    cut=np.logical_and(xdata>=np.amin(roots),xdata<=np.amax(roots))
     xcut = xdata[cut]
     ycut = ydata[cut]
 
+    #polyfit for n=1
+    p1 = np.polyfit(xcut,ycut,1)
 
-    return
+    return p1
 
 def calculate_ohmig_res(xdata, ydata, j_C):
     permutation = xdata.argsort()
@@ -79,7 +81,7 @@ def show_plot():
 def plot_file(filename):
     file = Document.query.filter_by(filename=filename).first()
     xdata, ydata = readfile(filepath = filepath(filename), u_amp = file.amplification)
-    plt.plot(xdata,ydata,".b",label="U(I), amplification")# = "+file.amplification)
+    plt.plot(xdata,ydata,".b",label="U(I), amplification = "+str(file.amplification))
     plt.xlabel("I in mA")
     plt.ylabel("U in uV")
     plt.legend()
@@ -94,28 +96,28 @@ def plot_j_C(filename):
     if file.remove_offset ==True:
         for i in range(0,len(ydata)):
             ydata[i] -= p11[-1]
-        p11 = np.polyfit(xdata,ydata,11)
 
     if file.remove_ohm:
+        ohmic_res_p1 = ohmig_res(xdata,ydata)
         for i in range(0,len(ydata)):
-            ydata[i] -= p11[-2]*xdata[i]
-        p11 = np.polyfit(xdata,ydata,11)
+            ydata[i] -= ohmic_res_p1[0]*xdata[i]
 
+    p11 = np.polyfit(xdata,ydata,11)
     p = np.poly1d(p11)
     roots = solve_for_y_real(p11, p11[-1]+10)
     froots = filter_roots_by_range(roots = roots, min = np.amin(xdata), max = np.amax(xdata))
-    ohmic_res_p1= calculate_ohmig_res(xdata,ydata,froots[0])
+    ohmic_res_p1 = ohmig_res(xdata,ydata)
 
     #discrete derivate
-    dy = np.zeros(xrange.shape,np.float)
-    dy[0:-1] = np.diff(p(xrange))/np.diff(xrange)
-    dy[-1] = dy[-2]
+    #dy = np.zeros(xrange.shape,np.float)
+    #dy[0:-1] = np.diff(p(xrange))/np.diff(xrange)
+    #dy[-1] = dy[-2]
 
-    d2y = np.zeros(xrange.shape,np.float)
-    d2y[0:-1] = np.diff(dy)/np.diff(xrange)
-    d2y[-1] = d2y[-2]
+    #d2y = np.zeros(xrange.shape,np.float)
+    #d2y[0:-1] = np.diff(dy)/np.diff(xrange)
+    #d2y[-1] = d2y[-2]
 
-    plt.plot(xdata,ydata, ".b", label="U(I), amplification")# = "+str(file.amplification))
+    plt.plot(xdata,ydata, ".b", label="U(I), amplification "+str(file.amplification))
 
     plt.plot(xrange,p(xrange),"-r",label="polyfit: n=11")
 
@@ -123,9 +125,9 @@ def plot_j_C(filename):
     if len(froots)==1:
         plt.plot([froots[0]]*2,[np.amin(ydata),np.amax(ydata)],"--r", label = "j_C = %.3fmA"%froots[0])
 
-    plt.plot(xrange,xrange*ohmic_res_p1[0]+ohmic_res_p1[-1],"--g", label = "R = %.5fmOhm"%ohmic_res_p1[0])
-    plt.plot(xrange,dy,"--y", label = "derivate of p11")
-    plt.plot(xrange,d2y,"g", label = "2nd derivate of p11")
+    plt.plot(xrange,ohmic_res_p1[0]*xrange+ohmic_res_p1[-1],"--g", label = "R = %.5fmOhm"%ohmic_res_p1[0])
+    #plt.plot(xrange,dy,"--y", label = "derivate of p11")
+    #plt.plot(xrange,d2y,"g", label = "2nd derivate of p11")
     plt.xlabel('I in mA')
     plt.ylabel('U in uV')
     plt.legend()
